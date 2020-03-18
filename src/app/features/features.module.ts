48f4f9e90a32cb64aefbc90212e594f479435003
MehdiBekir93 <mehdi.bekir@aymax.fr>
2020-03-18 11:33:20 +0100

    delete import

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FeaturesRoutingModule } from "./features-routing.module";
 import { FeaturesComponent } from "./features.component";
