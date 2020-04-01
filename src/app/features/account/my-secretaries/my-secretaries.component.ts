cd8815096c74bd092942110b966cbb20b38e9532
Gabsi Wajih <wajih.gabsi@aymax.fr>
2020-04-01 18:07:46 +0200

    HLS-184 mes secretaires

--- src/app/features/account/my-secretaries/my-secretaries.component.ts
+++ src/app/features/account/my-secretaries/my-secretaries.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AccountService } from '@app/features/services/account.service';
-import { ContactsService } from '@app/features/services/contacts.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { AccountService } from "@app/features/services/account.service";
+import { ContactsService } from "@app/features/services/contacts.service";
 declare var $: any;
 @Component({
-  selector: 'app-my-secretaries',
-  templateUrl: './my-secretaries.component.html',
-  styleUrls: ['./my-secretaries.component.scss']
+  selector: "app-my-secretaries",
+  templateUrl: "./my-secretaries.component.html",
+  styleUrls: ["./my-secretaries.component.scss"]
 })
 export class MySecretariesComponent implements OnInit {
+  isList = true;
+  isEdit: boolean;
   users: Array<any>;
   errorMessage = "";
   successMessage = "";
@@ -21,13 +28,15 @@ export class MySecretariesComponent implements OnInit {
   public infoForm: FormGroup;
   itemsList: Array<any> = [];
   imageSource = "assets/imgs/user.png";
-  constructor(public router: Router,
-              public accountService: AccountService,
-              private contactsService: ContactsService) {
+  constructor(
+    public router: Router,
+    public accountService: AccountService,
+    private contactsService: ContactsService
+  ) {
     this.messages = this.accountService.messages;
     this.labels = this.contactsService.messages;
     this.errors = this.accountService.errors;
-   }
+  }
 
   ngOnInit(): void {
     this.initInfoForm();
@@ -37,9 +46,13 @@ export class MySecretariesComponent implements OnInit {
     this.infoForm = new FormGroup({
       last_name: new FormControl(null, Validators.required),
       first_name: new FormControl(null, Validators.required),
-      email: new FormControl(null, {validators: [Validators.required, Validators.email]}),
+      email: new FormControl(null, {
+        validators: [Validators.required, Validators.email]
+      }),
       civility: new FormControl(null, Validators.required),
-      phone: new FormControl(null, {validators: [Validators.required, Validators.pattern("[0-9]*")]}),
+      phone: new FormControl(null, {
+        validators: [Validators.required, Validators.pattern("[0-9]*")]
+      }),
       picture: new FormControl(null)
     });
   }
@@ -49,59 +62,69 @@ export class MySecretariesComponent implements OnInit {
   close() {
     this.showAlert = false;
   }
+  addSecretaryButton() {
+    this.infoForm.reset();
+    this.isEdit = false;
+    this.isList = !this.isList;
+  }
   submit() {
     this.submitted = true;
     if (this.infoForm.invalid) {
       return;
     }
     const model = {
-        email: this.infoForm.value.email,
-        phoneNumber: this.infoForm.value.phone,
-        firstName: this.infoForm.value.first_name,
-        lastName: this.infoForm.value.last_name,
-        photoId: this.infoForm.value.picture,
-        civility: this.infoForm.value.civility,
+      email: this.infoForm.value.email,
+      phoneNumber: this.infoForm.value.phone,
+      firstName: this.infoForm.value.first_name,
+      lastName: this.infoForm.value.last_name,
+      photoId: this.infoForm.value.picture,
+      civility: this.infoForm.value.civility
     };
-    this.accountService.addSecretary(model).subscribe(res => {
-      this.showAlert = true;
-      this.initInfoForm();
-      $('.alert').alert();
-      this.submitted = false;
-      this.getMySecretaries();
-    });
+    if (!this.isEdit) {
+      this.accountService.addSecretary(model).subscribe(res => {
+        this.showAlert = true;
+        this.initInfoForm();
+        $(".alert").alert();
+        this.submitted = false;
+        this.getMySecretaries();
+      });
+    } else {
+    }
   }
   return() {
-    this.router.navigate(['/features/messageries']);
+    this.router.navigate(["/features/messageries"]);
   }
-  getMySecretaries(){
-    this.accountService.getMySecretaries().subscribe(contacts => {
-      this.users = contacts;
-      this.itemsList = this.users.map(elm => {
-        return {
-          id: elm.id,
-          isSeen: true,
-          users: [{
+  getMySecretaries() {
+    this.accountService.getMySecretaries().subscribe(
+      contacts => {
+        this.users = contacts;
+        this.itemsList = this.users.map(elm => {
+          return {
             id: elm.id,
-            fullName: elm.fullName,
-            img: "assets/imgs/IMG_3944.jpg",
-            title: "Sc",
-            type: "MEDICAL"
-          }],
-          isImportant: false,
-          hasFiles: false,
-          isViewDetail: false,
-          isMarkAsSeen: false,
-          isChecked: false
-        };
-      });
-  },
-  error => {
-    console.log("error");
+            isSeen: true,
+            users: [
+              {
+                id: elm.id,
+                fullName: elm.fullName,
+                img: "assets/imgs/IMG_3944.jpg",
+                title: "Sc",
+                type: "MEDICAL"
+              }
+            ],
+            isImportant: false,
+            hasFiles: false,
+            isViewDetail: false,
+            isMarkAsSeen: false,
+            isChecked: false
+          };
+        });
+      },
+      error => {
+        console.log("error");
+      }
+    );
   }
-  );
-  }
-  cardClicked(event){}
- selectItem(event){}
- deleteActionClicked(){}
-
+  cardClicked(event) {}
+  selectItem(event) {}
+  deleteActionClicked() {}
 }
