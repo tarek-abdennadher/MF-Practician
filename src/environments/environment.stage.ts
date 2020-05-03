5777a570fbf0f1f47b94e1950d2a64545ce9fb9d
neylihajer <hajer.neyli@aymax.fr>
2020-05-03 22:36:25 +0100

    HLS-55 Add link to public site and demonstration for doctors

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   BASE_END_POINT: "http://51.178.83.125:8765",
-  PATIENT_SITE: "http://51.178.83.125:4200"
+  PATIENT_SITE: "http://51.178.83.125:4200",
+  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/"
 };
