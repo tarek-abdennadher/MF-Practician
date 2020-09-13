05d60af06d11ab764ea1756d602bac658d7aac3e
neylihajer <hajer.neyli@aymax.fr>
2020-09-13 11:47:48 +0100

    HLS-1207 Ajout de la popup de la fiche patient

--- src/app/features/services/dialog.service.ts
+++ src/app/features/services/dialog.service.ts
@@ -1,12 +1,13 @@
 import { MatConfirmDialogComponent } from "./../mat-confirm-dialog/mat-confirm-dialog.component";
 import { Injectable } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
+import { MatPatientFileDialogComponent } from '../mat-patient-file-dialog/mat-patient-file-dialog.component';
 
 @Injectable({
   providedIn: "root"
 })
 export class DialogService {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) { }
 
   openConfirmDialog(msg, titre) {
     return this.dialog.open(MatConfirmDialogComponent, {
@@ -21,4 +22,19 @@ export class DialogService {
       }
     });
   }
+
+  openPatientFile(title, info) {
+    return this.dialog.open(MatPatientFileDialogComponent, {
+      width: "1000px",
+      height: "75%",
+      panelClass: "patient-file-container",
+      disableClose: true,
+      position: { top: "120px" },
+      data: {
+        title: title,
+        info: info
+      },
+    });
+  }
+
 }
