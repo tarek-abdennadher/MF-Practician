f84c7a869a81c966464a64a2cc454f2c30710c87
Gabsi Wajih <wajih.gabsi@aymax.fr>
2020-09-20 15:11:44 +0000

    environment.prod.ts edited online with Bitbucket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_END_POINT: "http://20.188.60.23:8765",
+  PATIENT_SITE: "http://51.103.21.136:4200",
+  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/",
 };
