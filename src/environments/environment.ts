a9602dcbd1117c1149b884a7a081f596068831cc
mfadhlaoui <mortadha.fadhlaoui@aymax.fr>
2020-09-29 19:08:48 +0100

    delete the clean up from BehaviorSubject

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  BASE_END_POINT: "http://51.178.83.124:8765",
-  PATIENT_SITE: "http://51.178.83.124:4200",
-  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/",
+  BASE_END_POINT: "http://localhost:8765",
+  PATIENT_SITE: "http://localhost:4200",
+  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/"
 };
 
 /*
