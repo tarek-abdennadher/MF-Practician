547579c7b130e9dc7111438594952990a4b29e12
Ferdinand <youssef.ben-amor@aymax.fr>
2020-10-19 14:36:42 +0200

    HLS-777 401 errors - exclude wrong password error from redirection

--- src/app/core/interceptors/response.interceptor.ts
+++ src/app/core/interceptors/response.interceptor.ts
@@ -5,24 +5,33 @@ import { LocalStorageService } from 'ngx-webstorage';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { NotifierService } from 'angular-notifier';
+import { GlobalService } from '../services/global.service';
 
 @Injectable()
 export class ResponseInterceptor implements HttpInterceptor {
   private readonly notifier: NotifierService;
+  private excludedExceptions = [];
+
   constructor(
     private localSt: LocalStorageService,
     private router: Router,
+    private globalService: GlobalService,
     private notifierService: NotifierService,) {
     this.notifier = notifierService;
+    this.excludedExceptions = this.globalService.excludedExceptions;
   }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         switch (error.status) {
           case 401:
-            this.router.navigate(["/connexion"]);
+            if (error && error.error && error.error.apierror && this.excludedExceptions.includes(error.error.apierror.message)) {
+              return throwError(error);
+            } else {
+              this.router.navigate(["/connexion"]);
+            }
             break;
-          case 500 :
+          case 500:
             this.setNotif("Une erreur s’est produite et l'équipe travail dessus.", "error");
             break;
           default:
