753d21cf6387820848da976f9338139c089e07a0
mfadhlaoui <mortadha.fadhlaoui@aymax.fr>
2021-02-10 21:15:54 +0100

    fix search in my patients

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,25 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  // production: false,
-  // BASE_END_POINT: "http://localhost:8765",
-  // PATIENT_SITE: "http://localhost:4200",
-  // site_Map:"https://www.helssy.io/plan-du-site/",
-  // WHOAREWE_SITE:"https://www.helssy.io/qui-sommes-nous/",
-  // OURSOLUTION_SITE:"https://www.helssy.io/notre-solution/",
-  // OURBLOG_SITE:"https://www.helssy.io/category/blog/",
-  // CONTACT_SITE:"https://www.helssy.io/contact/",
-  // HELSSY_SITE:"https://www.helssy.io/",
-  // CGU_SITE:"https://www.helssy.io/cgu/",
-  // RGPD_SITE:"https://www.helssy.io/rgpd/",
-  // COOKIES_SITE:"https://www.helssy.io/cookies/",
-  // SHOWCASE_SITE: "https://helssy.aymax.fr/contact/",
-  // APPSTORE_SITE:'https://apps.apple.com/us/app/helssy/id1541125435',
-  // PLAYSTORE_SITE:'https://play.google.com/store/apps/details?id=helssy.io',
-  // encryptKey: "q4t7w!z%"
-    production: false,
-  BASE_END_POINT: "http://51.178.83.124:8765",
-  PATIENT_SITE: "http://51.178.83.124:4200",
+  production: false,
+  BASE_END_POINT: "http://localhost:8765",
+  PATIENT_SITE: "http://localhost:4200",
   site_Map:"https://www.helssy.io/plan-du-site/",
   WHOAREWE_SITE:"https://www.helssy.io/qui-sommes-nous/",
   OURSOLUTION_SITE:"https://www.helssy.io/notre-solution/",
@@ -31,10 +15,9 @@ export const environment = {
   CGU_SITE:"https://www.helssy.io/cgu/",
   RGPD_SITE:"https://www.helssy.io/rgpd/",
   COOKIES_SITE:"https://www.helssy.io/cookies/",
+  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/",
   APPSTORE_SITE:'https://apps.apple.com/us/app/helssy/id1541125435',
   PLAYSTORE_SITE:'https://play.google.com/store/apps/details?id=helssy.io',
-  SHOWCASE_SITE: "https://helssy.aymax.fr/contact/",
-
   encryptKey: "q4t7w!z%"
 };
 
