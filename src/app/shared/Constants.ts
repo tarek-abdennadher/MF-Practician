25c2ed4681c5a76913c745bd544d39903279e691
mfadhlaoui <mortadha.fadhlaoui@aymax.fr>
2021-03-12 15:44:27 +0100

    add pdf viewer

--- src/app/shared/Constants.ts
+++ src/app/shared/Constants.ts
@@ -1,2 +1,4 @@
-export const VALIDFILEEXTENSIONS = ["jpg", "jpeg", "bmp", "gif", "png", "svg"];
+export const VALIDIMAGEEXTENSIONS = ["jpg", "jpeg", "bmp", "gif", "png", "svg"];
+export const VALIDFILEEXTENSIONS = ["jpg", "jpeg", "bmp", "gif", "png", "svg", "pdf"];
 export const SVG = "svg";
+export const PDF = "pdf";
